24eca0b86707c781a3bae8a3c92b54e435d4841f
Dejana Pivac <dejanapivac@gmail.com>
2024-08-16 16:30:47 +0200

    db configuration

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+""""
+Claculator functions
+"""
+
+def add(x, y):
+    """Add x and y and return the result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return res"""
+    return y - x
