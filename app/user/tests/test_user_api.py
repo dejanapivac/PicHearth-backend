38ade2afdff2fdb2fbcf817baa609a800f3cb2bf
Dejana Pivac <dejanapivac@gmail.com>
2024-08-23 15:40:36 +0200

    create user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -49,7 +49,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_password_too_short(self):
+    def test_password_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
         payload = {
             'email': 'test@example.com',
